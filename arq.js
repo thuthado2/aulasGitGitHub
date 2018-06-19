92de5c105fb598fadfbcc2525da0993c9d4696b8
Mateus moitinho <matheus_mos.2@hotmail.com>
2018-06-19 06:59:27 -0300

    segundo commit do projeto: somar os dois inteiros informado pleo usuário.

---
+++ arq.js
@@ -0,0 +1,10 @@
+fution soma () {
+   var n1, n2;
+
+   n1 = document.getElementById ("primeiroNumero").value;
+   n2 = document.getElementById ("segundoNumero") .value;
+   n1 = parseInt (n1);
+   n2 = parseInt (n2);
+    soma = n1 + n2;
+}
+
